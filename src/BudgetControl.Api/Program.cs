cda7f8d90f96e6cc13e85974d4c4b1d32445ac2a
igor.andrade <profissional@igorandrade.com>
2025-01-29 19:19:53 -0300

    Adicionando Swager

--- src/BudgetControl.Api/Program.cs
+++ src/BudgetControl.Api/Program.cs
@@ -9,13 +9,27 @@ builder.Services.AddSingleton<WeatherForecastService>();
 
 // Configuração do Swagger
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "BudgetControl API",
+        Version = "v1",
+        Description = "Uma API para controle de orçamento pessoal",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Igor Andrade",
+            Email = "seuemail@exemplo.com",
+            Url = new Uri("https://www.seusite.com")
+        }
+    });
+});
 
 var app = builder.Build();
 
-
+app.UseSwagger(); // Ativa a geração do Swagger
+app.UseSwaggerUI(); // Ativa a interface gráfica para visualizar a documentação
 app.UseHttpsRedirection();
-
-// Mapeamento dos Endpoints
 app.MapWeatherForecastEndpoints();
 
 app.Run();
